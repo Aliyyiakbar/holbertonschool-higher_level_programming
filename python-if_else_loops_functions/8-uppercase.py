0071673f2cf7964b7f0240799a758c7a67df2da2
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-18 13:45:57 +0400

    Refactor uppercase function to use index-based iteration for clarity

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        print("{}".format(chr(ord(c) - (32 if ord('a') <= ord(c) <= ord('z') else 0))), end='')
+    for i in range(len(str)):
+        print("{}".format(chr(ord(str[i]) - (32 if ord('a') <= ord(str[i]) <= ord('z') else 0))), end=("" if i != len(str) - 1 else "\n"))
