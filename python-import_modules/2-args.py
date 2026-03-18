5100c2b259500339e5687fba783369e386741798
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-18 16:18:38 +0400

    Add initial implementation of argument printing script

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    if len(argv) <= 1:
+        print("0 arguments.")
+        return
+
+    print("{} argument:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
