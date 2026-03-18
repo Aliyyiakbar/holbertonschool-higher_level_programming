433b686cf59dc323139198377fdcbe54d8e325e8
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-18 13:44:58 +0400

    Fix uppercase function to print characters on the same line

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        print("{}".format(chr(ord(c) - (32 if ord('a') <= ord(c) <= ord('z') else 0))))
+        print("{}".format(chr(ord(c) - (32 if ord('a') <= ord(c) <= ord('z') else 0))), end='')
