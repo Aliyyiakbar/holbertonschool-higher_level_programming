032992959cc8b9105e0a3f2acbb7c1441bd857f6
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-18 18:41:03 +0400

    Refactor hidden discovery script to read and print contents of hidden_4.pyc file

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-import builtins
-
-
 def main():
-    a = dir("hidden_4.pyc")
-    b = []
-
-    for i in sorted(a):
-        if i[0] == '_':
-            continue
-        b.append(i)
-
-    print(*b, sep='\n')
+    a = []
+    with open("hidden_4.pyc", encoding="latin1") as f:
+        print(f.read())
 
 
 if __name__ == "__main__":
