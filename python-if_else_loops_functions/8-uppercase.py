8980be13ee5377422a5eaf3d644f00d90405fe54
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-18 13:47:10 +0400

    Add check for empty string in uppercase function to handle edge case

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
+    if (len(str) == 0):
+        print()
+        return
+
     for i in range(len(str)):
         print("{}".format(chr(ord(str[i]) - (32 if ord('a') <= ord(str[i]) <= ord('z') else 0))), end=("" if i != len(str) - 1 else "\n"))
