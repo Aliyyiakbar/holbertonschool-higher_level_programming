0fe6440bc4a27ed6409f750849f12f3fd6dd1b82
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-19 01:32:37 +0400

    Add calculator script for basic arithmetic operations

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        return 1
+
+    if argv[2] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /\n")
+        return 1
+
+    a, b = int(argv[1]), int(argv[3])
+
+    if argv[2] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif argv[2] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
