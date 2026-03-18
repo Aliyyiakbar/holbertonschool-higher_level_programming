6446f292e1b5c17b8575bea96ce3ac6f74ca9ae0
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-18 16:20:51 +0400

    Fix singular/plural mismatch in argument count output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ def main():
         print("0 arguments.")
         return
 
-    print("{} arguments:".format(len(argv) - 1))
+    print("{} argument{}:".format(len(argv) - 1, ('s' if len(argv) > 2 else '')))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
 
