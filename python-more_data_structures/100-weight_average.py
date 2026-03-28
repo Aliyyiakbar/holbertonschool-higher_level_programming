076bd640e6bc9f013f3b85b05c8191845ece516e
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-28 18:53:58 +0400

    Refactor 100-weight_average.py to use x, y values in loops.

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     s, c = 0, 0
-    for i in my_list:
-        s += i[0] * i[1]
-        c += i[1]
+    for x, y in my_list:
+        s += x * y
+        c += y
 
     return 0 if c == 0 else s / c
