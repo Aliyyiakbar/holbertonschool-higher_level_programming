67548e83600976d5363f6b81619b7fca8ef6c773
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-29 23:14:21 +0400

    Refactor safe_print_list function to improve exception handling structure

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     c = 0
-
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end='')
             c += 1
 
-        print()
-        return c
+        except IndexError:
+            break
 
-    except IndexError:
-        print()
-        return c
+    print()
+    return c
