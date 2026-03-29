1f0e75dfa37eb32bf780bc801fe203b938d49f4c
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-29 23:12:11 +0400

    Add safe_print_list function to safely print elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            c += 1
+
+        print()
+        return c
+
+    except IndexError:
+        print()
+        return c
