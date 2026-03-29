3eea7c1a0292cda75df980a0ea0cb7aae949dc0b
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-03-29 23:36:22 +0400

    Add list_division function to perform safe division on two lists and handle exceptions

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(list_length):
+        try:
+            res.append(my_list_1[i] / my_list_2[i])
+
+        except Exception as e:
+            if isinstance(e, ZeroDivisionError):
+                print("division by 0")
+
+            elif isinstance(e, TypeError):
+                print("wrong type")
+
+            elif isinstance(e, IndexError):
+                print("out of range")
+
+            res.append(0)
+
+    return res
