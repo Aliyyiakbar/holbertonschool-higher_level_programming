2ae99093ce5d1e0b80c2de1777a7fc0db614d930
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-04-03 12:40:59 +0400

    Refactor magic_calculation function to improve exception handling structure

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -3,9 +3,13 @@ def magic_calculation(a, b):
     res = 0
 
     for i in range(1, 3):
-        if i > a:
-            res = b + a
-            raise Exception("Too far")
+        try:
+            if i > a:
+                res = b + a
+                raise Exception("Too far")
+
+        except:
+            break
 
         res += a ** b / i
 
