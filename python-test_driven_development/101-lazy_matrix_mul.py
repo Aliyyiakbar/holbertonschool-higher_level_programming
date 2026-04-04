96a24087cd3ccfd17c284256f5e2be256020f4f4
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-04-04 16:14:54 +0400

    Refactor lazy_matrix_mul function to enhance error handling for scalar operands and improve matrix multiplication for specific data types

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,4 +9,10 @@ def lazy_matrix_mul(m_a, m_b):
     a = np.array(m_a)
     b = np.array(m_b)
 
+    if a.ndim == 0 or b.ndim == 0:
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
+
+    if a.dtype.kind in "OUS" or b.dtype.kind in "OUS":
+        return np.einsum('ij,jk->ik', a, b)
+
     return np.matmul(a, b)
