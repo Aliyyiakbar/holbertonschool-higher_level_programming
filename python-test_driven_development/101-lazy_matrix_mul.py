640358a058655db933681ae6402194844c0304b4
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-04-04 17:49:49 +0400

    Refactor lazy_matrix_mul function to implement error handling and support for scalar operands

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,4 +6,18 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Return the product of 2 matrices."""
-    return np.matmul(m_a, m_b)
+    try:
+        a = np.array(m_a)
+        b = np.array(m_b)
+    except ValueError as e:
+        if str(e).startswith("setting an array element with a sequence"):
+            raise ValueError("setting an array element with a sequence.")
+        raise
+
+    if a.ndim == 0 or b.ndim == 0:
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
+
+    if a.dtype.kind in "OUS" or b.dtype.kind in "OUS":
+        return np.einsum('ij,jk->ik', a, b)
+
+    return np.dot(a, b)
