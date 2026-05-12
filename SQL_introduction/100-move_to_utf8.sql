c8f0d2ae05b52950b559de3d77801e151251ad1e
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-05-13 01:03:32 +0400

    Refactor SQL script to convert database and table to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,4 @@
--- Convert hbtn_0c_0 database, first_table and name field to UTF8
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert first_table default charset to UTF8
-ALTER TABLE hbtn_0c_0.first_table
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert name field to UTF8
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(256)
-COLLATE utf8mb4_unicode_ci
-DEFAULT NULL;
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
