e5f3f3dba495a1e1cbf3e8a32e6b8be60c4f4794
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-05-13 00:59:05 +0400

    Add comments to clarify SQL script for converting database and table to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,17 @@
+-- Convert database to UTF8
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
+
+-- Select database
 USE hbtn_0c_0;
+
+-- Convert table to UTF8
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
+
+-- Convert field name to UTF8
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
