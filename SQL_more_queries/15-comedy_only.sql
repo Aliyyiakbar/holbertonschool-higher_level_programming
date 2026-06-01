1def0be55251835672ce276cc556df156abb058e
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-06-01 08:49:07 +0400

    Add SQL script to retrieve genres for shows titled 'Comedy' from tv_genres and tv_show_genres tables

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- 15-comedy_only.sql
+
+SELECT tv_genres.name
+FROM tv_genres, tv_show_genres, tv_shows
+WHERE tv_genres.id = tv_show_genres.genre_id
+  AND tv_shows.id = tv_show_genres.show_id
+  AND tv_shows.title = 'Comedy'
+ORDER BY tv_genres.name ASC;
